2b460d4b87c0c95ecde7829e50366bfa31c54d90
Alex Ellis <alexellis2@gmail.com>
2019-04-25 12:49:30 +0100

    Add stack.yml
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace aspnetcoreapp
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("http://0.0.0.1:8080/");
+                .UseUrls("http://0.0.0.0:8080/");
 
     }
 }
