b9fb600a2d4780066a4b998fe95c350774cbdfb8
Alex Ellis <alexellis2@gmail.com>
2019-04-25 12:41:46 +0100

    Initial
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetcoreapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.IO.File.WriteAllText("/tmp/.lock", "");
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.1:8080/");
+
+    }
+}
